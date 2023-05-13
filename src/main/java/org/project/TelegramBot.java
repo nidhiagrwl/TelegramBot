938330f0cd7736161847a52397b63341fc95a33e
Nidhiagr <nidhi.agrawal@jobtwine.com>
2023-05-13 17:55:06 +0530

    echo bot

---
+++ src/main/java/org/project/TelegramBot.java
@@ -0,0 +1,34 @@
+package org.project;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class TelegramBot extends TelegramLongPollingBot {
+
+
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            SendMessage message = new SendMessage(); // Create a SendMessage object with mandatory fields
+            message.setChatId(update.getMessage().getChatId().toString());
+            message.setText(update.getMessage().getText());
+
+            try {
+                execute(message); // Call method to send the message
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    public String getBotUsername() {
+        return "NidhiagrBot";
+    }
+    @Override
+    public String getBotToken() {
+        return "6268005128:AAGE1fGxb5i3SCYVfpivlGExI5bL-WxWbyA";
+    }
+
+}
